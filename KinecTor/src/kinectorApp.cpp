4f6490c247f14082ca517cef92d853108055c022
Davide <davide.rambaldi@gmail.com>
2012-07-24 17:30:56 +0200

    kinectorApp files

---
+++ KinecTor/src/kinectorApp.cpp
@@ -0,0 +1,9 @@
+//
+//  kinectorApp.cpp
+//  KinectTor
+//
+//  Created by drambald on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ KinecTor/src/kinectorApp.h
@@ -0,0 +1,15 @@
+//
+//  kinectorApp.h
+//  KinectTor
+//
+//  Created by drambald on 7/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxKinect.h"
+
+
