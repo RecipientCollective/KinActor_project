d1ff19cc6c44df506e0e9871fc6555ff307eb6c0
Davide <davide.rambaldi@gmail.com>
2012-07-24 17:34:29 +0200

    removing testapp

--- KinecTor/src/kinectorApp.cpp
+++ KinecTor/src/kinectorApp.cpp
@@ -6,4 +6,64 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#include <iostream>
+#include <kinectorApp.h>
+
+//--------------------------------------------------------------
+void kinectorApp::setup(){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::update(){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::draw(){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::mouseMoved(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void kinectorApp::dragEvent(ofDragInfo dragInfo){ 
+    
+}
